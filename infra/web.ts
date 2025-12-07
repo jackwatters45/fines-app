f56197ad90f71d76d5cc5011778443f8626d6b4d
jackwatters45 <jack.watters@me.com>
2025-12-07 19:46:48 +1100

    feat: add kv + storage

--- infra/web.ts
+++ infra/web.ts
@@ -1,9 +1,13 @@
 import { TanStackStart } from 'alchemy/cloudflare';
 import { database } from './database';
+import { kv } from './kv';
+import { storage } from './storage';
 
 export const web = await TanStackStart('web', {
   bindings: {
     DB: database,
+    KV: kv,
+    STORAGE: storage,
   },
   cwd: './packages/web',
 });
